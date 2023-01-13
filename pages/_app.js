dff2d40cb29a7d3386dcf0129d21685aac4db619
Aarni Huuskonen <aarnihuuskonen@Aarnis-MBP.home>
2023-01-13 19:29:23 +0200

    initial

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+//import '@/styles/globals.css'
+import { StarknetConfig, InjectedConnector } from '@starknet-react/core'
+
+export default function MyApp({ Component, pageProps }) {
+  const connectors = [
+    new InjectedConnector({ options: { id: 'braavos' }}),
+    new InjectedConnector({ options: { id: 'argentX' }}),
+  ]
+  return (
+    <StarknetConfig connectors={connectors}>
+      <Component {...pageProps} />
+    </StarknetConfig>
+  )
+}
