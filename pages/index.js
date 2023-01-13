ebf7d8068407eb4b5a8ca9d468e585f49cf4638e
Pikkuherkko <juhhoh95@hotmail.com>
2023-01-13 20:33:06 +0200

    css

--- pages/index.js
+++ pages/index.js
@@ -15,41 +15,54 @@ const getTokenContract = async () => {
   return myTestContract;
 };
 
-
 const allow = async (account) => {
   const tokenContract = await getTokenContract();
-  tokenContract.connect(account)
+  tokenContract.connect(account);
   console.log(tokenContract);
   const allow = await tokenContract.invoke("request_allowlist");
-  
 };
 
 const getTokens = async (account) => {
   const tokenContract = await getTokenContract();
-  tokenContract.connect(account)
+  tokenContract.connect(account);
   const handle = await tokenContract.invoke("get_tokens");
 };
 
-
 export default function Home() {
-  const { account, address, status } = useAccount()
-  const {connect, connectors} = useConnectors()
+  const { account, address, status } = useAccount();
+  const { connect, connectors } = useConnectors();
   return (
-    <>
-    <h1>App</h1>
-    <ul>
-      {connectors.map((connector) => (
-        <li key={connector.id()}>
-          <button onClick={() => connect(connector)}>
-            Connect {connector.id()}
-          </button>
-        </li>
-      ))}
-    </ul>
     <div>
-      <button onClick={() => allow(account)}>permission</button>
-      <button onClick = {() =>getTokens(account)}>getTokens</button>
+      <div className="flex flex-row justify-between w-screen text-xl p-4">
+        <h1 className="text-3xl font-bold">Token App</h1>
+        <div className="flex flex-row">
+          {connectors.map((connector) => (
+            <div key={connector.id()} className="">
+              <button
+                onClick={() => connect(connector)}
+                className="bg-orange-500 ml-2 p-2 rounded-xl hover:bg-black hover:text-orange-500"
+              >
+                Connect {connector.id()}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col mt-60 align-middle items-center">
+        <button
+          onClick={() => allow(account)}
+          className="bg-orange-500 p-2 w-fit rounded-xl animate-bounce font-bold hover:bg-black hover:text-orange-500"
+        >
+          Get whitelisted
+        </button>
+        <button
+          onClick={() => getTokens(account)}
+          className="bg-orange-500 p-2 w-fit rounded-xl mt-8 font-bold hover:bg-black hover:text-orange-500"
+        >
+          Mint Tokens!
+        </button>
+      </div>
     </div>
-    </>
-  )
+  );
 }
